825f20880f4aa74af83d5d4f57d9435aa015d1cb
ChoiHaesol <enfzls1212@gmail.com>
2019-05-18 13:23:39 +0900

    first commit

---
+++ exam01.py
@@ -0,0 +1,27 @@
+if 3<2:
+    print('3 is more than 2')
+elif 4<3:
+    print('4 is more than 3')
+else :
+    print('none')
+
+    #웅앵웅
+    '''dndlsdkfja;ewjf;lk'''
+
+def tmp():
+    print('함수 만들기')
+
+tmp()
+
+def tmp2(name):
+    print(name, 'hi')
+
+tmp2('dory')
+
+names = [1,2,3]
+
+for i in names:
+    print(i)
+
+for i in range(1,11,2):
+    print(i)
